4ff520f037a406c4c2532b1d6fa39dc2a6b340c6
jasonn361 <79726650+jasonn361@users.noreply.github.com>
2023-03-28 02:55:01 -0700

    Added animation to notifs

--- .config/nvim/lua/huy/plugins/nvim-notify.lua
+++ .config/nvim/lua/huy/plugins/nvim-notify.lua
@@ -1,12 +1,18 @@
 return {
   'rcarriga/nvim-notify',
-  opts = {
-    timeout = 3000,
-    max_height = function()
-      return math.floor(vim.o.lines * 0.5)
-    end,
-    max_width = function()
-      return math.floor(vim.o.columns * 0.5)
-    end,
-  },
+
+  config = function()
+    require('notify').setup{{
+      stages = 'fade_in_slide_out',
+      background_colour = '#000000',
+      timeout = 3000,
+      max_height = function()
+        return math.floor(vim.o.lines * 0.5)
+      end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.5)
+      end,
+    }}
+    vim.notify = require('notify')
+  end
 }
