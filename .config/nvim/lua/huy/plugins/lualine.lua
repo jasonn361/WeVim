14713109f30d7d1b489400fabbc2acb617d65454
jasonn361 <79726650+jasonn361@users.noreply.github.com>
2023-03-28 02:37:11 -0700

    lualine ignores non-editable buffers

--- .config/nvim/lua/huy/plugins/lualine.lua
+++ .config/nvim/lua/huy/plugins/lualine.lua
@@ -7,9 +7,17 @@ return {
     require('lualine').setup({
       -- Lualine Detail
       options = {
-       theme = require('lualine.themes.moonfly'),
-       section_separators = '',
-       component_separators = '|',
+        theme = require('lualine.themes.moonfly'),
+        section_separators = '',
+        component_separators = '|',
+        disabled_filetypes = {
+        statusline = {
+          'neo-tree',
+          'lazy',
+          'dashboard',
+          'alpha',
+        }
+       }
       },
 
       sections = {
